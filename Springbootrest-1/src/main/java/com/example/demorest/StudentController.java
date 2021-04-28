6f9501ddebffe22487ee889e0c2194b8b63e1958
abdul <abdul.tanveer@gmail.com>
2021-04-28 14:44:25 +0530

    initial commit

---
+++ Springbootrest-1/src/main/java/com/example/demorest/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.demorest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	StudentService studentService;
+	
+	@RequestMapping("students")
+	public List<Student> getStudents(){
+		
+		return studentService.getStudents();
+		
+	}
+
+}
