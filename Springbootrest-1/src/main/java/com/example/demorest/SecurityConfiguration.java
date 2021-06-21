009b34c3f7c3fe5279ad661cecfe3662faec1df9
abdul <abdul.tanveer@gmail.com>
2021-06-21 14:53:43 +0530

    added 2 users to  db -- jdbc

--- Springbootrest-1/src/main/java/com/example/demorest/SecurityConfiguration.java
+++ Springbootrest-1/src/main/java/com/example/demorest/SecurityConfiguration.java
@@ -1,32 +1,41 @@
 package com.example.demorest;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//set the config on the auth object
-		auth.inMemoryAuthentication()
-		.withUser("navneetu")
-		.password("nitigna")
-		.roles("USER")
-		.and()
-		.withUser("chandra")
-		.password("akshay")
-		.roles("ADMIN");
+		auth.jdbcAuthentication()
+		.dataSource(dataSource) //we have spring security to point to the h2 db
+		.withDefaultSchema()  //users n authorities
+		.withUser(
+				User.withUsername("user").password("userpwd").roles("USER")
+				)
+		.withUser(
+				User.withUsername("admin").password("adminpwd").roles("ADMIN")
+				);
+		
+		
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return NoOpPasswordEncoder.getInstance(); //use some hash encoder -- sha256
 	}
 	
 	@Override
@@ -35,7 +44,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/admin").hasRole("ADMIN")
 		.antMatchers("/user").hasAnyRole("USER","ADMIN")//please allow access to /user url to both user and admin
 		.antMatchers("/").permitAll()
-		//.hasAnyRole("_","_")
 		.and()
 		.formLogin();
 	}
